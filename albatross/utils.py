028b75668d3efb50fcf2bd36ccf2120435b4ad0c
Cameron Irmas <cirmas@protonmail.com>
2022-01-25 11:59:54 -0800

    [ref #2] Define WTK configurations
    This .yml file defines all current configurations for WTK file requests,
    and will assist with request structuring and validation.

---
+++ albatross/utils.py
@@ -0,0 +1,8 @@
+import pkgutil
+from yaml import load, Loader
+
+WTK_FILE = './wtk.yml'
+
+def _load_wtk(file=WTK_FILE):
+    data = pkgutil.get_data(__name__, "wtk.yml")
+    return load(data, Loader=Loader)
