8a7339e876a24cf53d9bdb633af365a174017202
Cameron Irmas <cirmas@protonmail.com>
2022-02-01 11:04:13 -0800

    [ref #2] Implement point data request
    Implements `request_wtk_point_data`, which requests data from NREL HSDS
    and formats the results into a `DataFrame`.

--- albatross/requests.py
+++ albatross/requests.py
@@ -1,7 +1,23 @@
 import json
+from rex import WindX
+import pandas as pd
+
 from .utils import _load_wtk
 
-def _build_url(region, year, resolution=None):
+
+def build_wtk_filepath(region, year, resolution=None):
+    """
+    A utility for building WIND Toolkit filepaths.
+
+    Args:
+        region (str): region in which the lat/lon point is located (see
+          `get_regions`)
+        year (int): year to be accessed (see `get_regions`)
+        resolution (:obj:`str`, optional): data resolution (see `get_regions`)
+
+    Returns:
+        str: The filepath for the requested resource.
+    """
     wtk = _load_wtk()
     base_url = '/nrel/wtk/'
 
@@ -9,7 +25,7 @@ def _build_url(region, year, resolution=None):
 
     year_range = wtk[region]['year_range']
     year_range = range(year_range[0], year_range[1]+1)
-
+    assert isinstance(year, int), '"year" must be an integer'
     msg = 'year %s not available for region: %s' % (year, region)
     assert year in year_range, msg
 
@@ -33,10 +49,67 @@ def _build_url(region, year, resolution=None):
     return base_url + url_region + file
 
 
-def request_wind_data(region, year, resolution=None):
-    url = _build_url(region, year, resolution)
+def request_wtk_point_data(region, year, lat_lon, params, resolution=None,
+                            tree=None, unscale=True, str_decode=True,
+                            group=None, hsds=True):
+    """
+    Requests WIND Toolkit data from NREL HSDS.
 
-    return url
+    Args:
+        region (str): region in which the lat/lon point is located (see
+          `get_regions`)
+        year (int): year to be accessed (see `get_regions`)
+        lat_lon (:obj:`list` of :obj:`float`): latitude/longitude point to
+          access
+        params (:obj:`list` of :obj:`str`): A list of parameters to include in
+          the dataset
+        resolution (:obj:`str`, optional): data resolution (see `get_regions`)
+        tree (:obj:`str`, optional): cKDTree or path to .pkl file containing
+          pre-computed tree of lat, lon coordinates, by default None
+        unscale (:obj:`bool`, optional): Boolean flag to automatically unscale
+          variables on extraction, by default True
+        str_decode (:obj:`bool`, optional): Boolean flag to decode the
+          bytestring meta data into normal strings. Setting this to False will
+          speed up the meta data read. by default True
+        group (:obj:`str`, optional): Group within .h5 resource file to open,
+          by default None
+        hsds (:obj:`bool`, optional): Boolean flag to use h5pyd to handle .h5
+          'files' hosted on AWS behind HSDS, by default True
+
+    Returns:
+        tuple: A tuple `(data, metadata)` consisting of a `DataFrame` and associated
+        metadata.
+    """
+    wtk_file = build_wtk_filepath(region, year, resolution)
+
+    assert isinstance(lat_lon, (list, tuple)), 'lat_lon must be a tuple or list'
+    err_msg = 'lat/lon points must be floats'
+    assert all([isinstance(x, float) for x in lat_lon]), err_msg
+    assert len(lat_lon) == 2, 'lat_lon must have a length of 2'
+
+    assert isinstance(params, (list, tuple)), '"params" must be a tuple or list'
+    assert len(params) != 0, '"params" must not be empty'
+    err_msg = '"params" elements must be strings'
+    assert all([isinstance(x, str) for x in params]), err_msg
+
+    kwargs = {
+        'tree': tree, 'unscale': unscale, 'str_decode': str_decode,
+        'group': group, 'hsds': hsds
+    }
+
+    results = []
+
+    with WindX(wtk_file, **kwargs) as f:
+        for param in params:
+            meta = f.meta
+            res = f.get_lat_lon_df(param, lat_lon)
+            col = res.columns[0]
+            res.rename(columns={col: param}, inplace=True)
+            results.append(res)
+
+    data = pd.concat(results, axis=1)
+
+    return (data, meta)
 
 
 def get_regions(pprint=False):
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,75 +1,152 @@
 import pytest
-from albatross.requests import request_wind_data, get_regions
+from albatross.requests import request_wtk_point_data, get_regions, build_wtk_filepath
 from albatross.utils import _load_wtk
 
 
-def test_request_wind_data_regions():
-    """Test valid region inputs for `request_wind_data`."""
+lat_lon = (39.913561, -105.222422)
+params = ['windspeed_90m']
+
+
+def test_build_wtk_filepath():
+    """Test valid inputs for `build_wtk_filepath`."""
+
     # regular region
-    res = request_wind_data('conus', 2007)
+    res = build_wtk_filepath('conus', 2007)
     assert res == '/nrel/wtk/conus/wtk_conus_2007.h5'
 
     # region with alternate naming
-    res = request_wind_data('hawaii', 2007)
+    res = build_wtk_filepath('hawaii', 2007)
     assert res == '/nrel/wtk/hawaii/Hawaii_2007.h5'
 
 
-def test_request_wind_data_invalid_regions():
-    """Test invalid region inputs for `request_wind_data`."""
+def test_build_wtk_filepath_invalid_regions():
+    """Test invalid `region` inputs for `build_wtk_filepath`."""
     with pytest.raises(AssertionError) as e:
-        request_wind_data('namek', 2007)
+        build_wtk_filepath('namek', 2007)
     assert str(e.value) == 'region not found: namek' 
 
 
-def test_request_wind_data_years():
-    """Test valid year inputs for `request_wind_data`."""
+def test_build_wtk_filepath_years():
+    """Test valid `year` inputs for `build_wtk_filepath`."""
     # beginning of year range
-    res = request_wind_data('canada', 2007)
+    res = build_wtk_filepath('canada', 2007)
     assert res == '/nrel/wtk/canada/wtk_canada_2007.h5'
 
     # end of year range
-    res = request_wind_data('canada', 2014)
+    res = build_wtk_filepath('canada', 2014)
     assert res == '/nrel/wtk/canada/wtk_canada_2014.h5'
 
     # between year range
-    res = request_wind_data('canada', 2010)
+    res = build_wtk_filepath('canada', 2010)
     assert res == '/nrel/wtk/canada/wtk_canada_2010.h5'
 
 
-def test_request_wind_data_invalid_year():
-    """Test invalid year inputs for `request_wind_data`."""
+def test_build_wtk_filepath_invalid_year():
+    """Test invalid `year` inputs for `build_wtk_filepath`."""
     with pytest.raises(AssertionError) as e:
-        request_wind_data('canada', 2015)
+        build_wtk_filepath('canada', 2015)
 
     msg = 'year %s not available for region: %s' % (2015, 'canada')
     assert str(e.value) == msg
 
+    with pytest.raises(AssertionError) as e:
+        build_wtk_filepath('canada', '2015')
 
-def test_request_wind_data_resolutions():
-    """Test valid resolution inputs for `request_wind_data`."""
+    msg = '"year" must be an integer'
+    assert str(e.value) == msg
+
+
+def test_build_wtk_filepath_resolutions():
+    """Test valid `resolution` inputs for `build_wtk_filepath`."""
     # no input defaults to "hourly"
-    res = request_wind_data('mexico', 2010)
+    res = build_wtk_filepath('mexico', 2010)
     assert res == '/nrel/wtk/mexico/wtk_mexico_2010.h5'
 
     # "hourly" can be explicitly specified
-    res = request_wind_data('mexico', 2010, 'hourly')
+    res = build_wtk_filepath('mexico', 2010, resolution='hourly')
     assert res == '/nrel/wtk/mexico/wtk_mexico_2010.h5'
 
     # "5min"
-    res = request_wind_data('mexico', 2010, '5min')
+    res = build_wtk_filepath('mexico', 2010, resolution='5min')
     assert res == '/nrel/wtk/mexico-5min/wtk_mexico_2010.h5'
 
 
-def test_request_wind_data_invalid_resolution():
-    """Test valid resolution inputs for `request_wind_data`."""
+def test_build_wtk_filepath_invalid_resolution():
+    """Test valid `resolution` inputs for `build_wtk_filepath`."""
     with pytest.raises(AssertionError) as e:
-        request_wind_data('canada', 2010, "1min")
+        build_wtk_filepath('canada', 2010, resolution='1min')
 
     msg = 'resolution "%s" not available for region: %s' % (
         '1min', 'canada')
     assert str(e.value) == msg
 
 
-def test_get_regions(pprint=False):
+def test_get_regions():
+    """Simply test `get_regions`."""
     wtk = _load_wtk()
     assert get_regions() == wtk
+
+
+def test_request_wtk_point_data_invalid_lat_lon():
+    # wrong type
+    lat_lon = 'bad'
+
+    with pytest.raises(AssertionError) as e:
+        request_wtk_point_data(
+            'canada', 2010, lat_lon, params, resolution='5min')
+
+    msg = 'lat_lon must be a tuple or list'
+    assert str(e.value) == msg
+
+    # wrong element type
+    lat_lon = ['bad']
+
+    with pytest.raises(AssertionError) as e:
+        request_wtk_point_data(
+            'canada', 2010, lat_lon, params, resolution='5min')
+
+    msg = 'lat/lon points must be floats'
+    assert str(e.value) == msg
+
+    # wrong number of elements
+    lat_lon = [1.0]
+
+    with pytest.raises(AssertionError) as e:
+        request_wtk_point_data(
+            'canada', 2010, lat_lon, params, resolution='5min')
+
+    msg = 'lat_lon must have a length of 2'
+    assert str(e.value) == msg
+
+
+def test_request_wtk_point_data_invalid_params():
+    """Test invalid `params` inputs for `request_wtk_point_data`"""
+    # wrong type
+    params = 'bad'
+
+    with pytest.raises(AssertionError) as e:
+        request_wtk_point_data(
+            'canada', 2010, lat_lon, params, resolution='5min')
+
+    msg = '"params" must be a tuple or list'
+    assert str(e.value) == msg
+
+    # empty params
+    params = []
+
+    with pytest.raises(AssertionError) as e:
+        request_wtk_point_data(
+            'canada', 2010, lat_lon, params, resolution='5min')
+
+    msg = '"params" must not be empty'
+    assert str(e.value) == msg
+
+    # wrong param type
+    params = [1]
+
+    with pytest.raises(AssertionError) as e:
+        request_wtk_point_data(
+            'canada', 2010, lat_lon, params, resolution='5min')
+
+    msg = '"params" elements must be strings'
+    assert str(e.value) == msg
